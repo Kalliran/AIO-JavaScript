e5633e6f60649e95651dd7fe119406142709d96d
kalliran <theautumn90@gmail.com>
2024-05-26 14:26:10 -0500

    changed files

---
+++ src/github.com/michaelcorleone/javascriptaio/script.js
@@ -0,0 +1,10 @@
+let phoneNumber = 2284243299;
+
+let phoneNumberString = phoneNumber.toString(); // "2284243299"
+let phoneNumberArray = phoneNumberString.split(""); // ["2", "2", "8", "4", "2", "4", "3", "2", "9", "9"]
+let phoneNumberArrayReversed = phoneNumberArray.reverse(); // [9, 9, 9, 2, 3, 4, 2, 4, 8, 2]
+let phoneNumberReversed = phoneNumberArrayReversed.join(""); // 9923424822
+
+const dt = new Date();
+const month = dt.getMonth();
+console.log(month);
