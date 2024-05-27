0bef8eb194b2c78cfbb6b66b3fdaccc9eef5937c
kalliran <theautumn90@gmail.com>
2024-05-27 11:50:26 -0500

    made changes to script.js and added files

--- src/github.com/michaelcorleone/javascriptaio/script.js
+++ src/github.com/michaelcorleone/javascriptaio/script.js
@@ -1,4 +1,4 @@
-let phoneNumber = 2284243299;
+let phoneNumber = "2284243299";
 
 let phoneNumberString = phoneNumber.toString(); // "2284243299"
 let phoneNumberArray = phoneNumberString.split(""); // ["2", "2", "8", "4", "2", "4", "3", "2", "9", "9"]
@@ -8,3 +8,8 @@ let phoneNumberReversed = phoneNumberArrayReversed.join(""); // 9923424822
 const dt = new Date();
 const month = dt.getMonth();
 console.log(month);
+
+for (let digit of phoneNumber) {
+  if (digit === "-") continue;
+  console.log(digit);
+}
