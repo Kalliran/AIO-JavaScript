c68851815a4086cf4f266e5428310212debad855
Michael Corleone <theautumn90@gmail.com>
2024-06-05 11:11:46 -0500

    added spread syntax to arrays

--- src/github.com/michaelcorleone/javascriptaio/randomNumberGuessingGame/script.js
+++ src/github.com/michaelcorleone/javascriptaio/randomNumberGuessingGame/script.js
@@ -13,3 +13,20 @@ for (let digit of phoneNumber) {
   if (digit === "-") continue;
   console.log(digit);
 }
+
+const person = ["Russell C. Guy", "3447 Twin House Lane", "Neosho", "MO"];
+
+let [personName, address, city, state] = person;
+
+const firstArray = ["a", "b", "c"];
+
+const secondArray = [...firstArray, "d"]; // ["a", "b", "c", "d"]
+// this is called a "shallow copy", because it contains just the values from the original array and has no reference to the original array.
+console.log(secondArray);
+
+const fruits = ["apple", "banana", "cherry"];
+const fruitsCopy = [...fruits]; // ["apple", "banana", "cherry"]
+console.log(fruitsCopy);
+fruitsCopy.push("watermelon"); // ["apple", "banana", "cherry", "watermelon"]
+console.log(fruitsCopy); // Now the new array is its own array and changes you make to it won't affect the original array.
+console.log(fruits);
